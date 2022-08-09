686357fb5952f31ecc9d7bd4742fa11276818db9
dwaynediddy <burrows.dwayne@icloud.com>
2022-08-09 13:31:54 +0200

    aws deploy

--- pages/components/PaymentForm.js
+++ pages/components/PaymentForm.js
@@ -1,56 +1,49 @@
-// import {
-//     CardElement,
-//     useStripe,
-//     useElements,
-//   } from "@stripe/react-stripe-js"
-//   import { useState } from 'react'
 
-// const PaymentForm = () => {
-//     const [name, setName] = useState('')
-//     const [email, setEmail] = useState('')
-//     // const stripe = useStripe()
-//     // const elements = useElements()
 
-//     const createSubscription = async () => {
-//         try {
-            
-//         } catch (err) {
-//             console.log(err)
-//             alert('payment failed', + err.message)
-//         }
-//     } 
-
-//   return (
-//     <div>
-//         Name: {''}
-//             <input 
-//                 text='text'
-//                 value={name}
-//                 onChange={(e) => setName(e.target.value)}
-//             />
-//             <br />
-//             Email: {''}
-//             <input 
-//                 text='text'
-//                 value={email}
-//                 onChange={(e) => setEmail(e.target.value)}
-//             />
-//             <br />
-//             <CardElement />
-
-//             <button onClick={createSubscription}>subscribe</button>
-//     </div>
-//   )
-// }
-
-// export default PaymentForm
-
-import React from 'react'
+import {
+    CardElement,
+    useElements,
+    useStripe,
+  } from "@stripe/react-stripe-js";
+  import { useState } from 'react'
 
 const PaymentForm = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const stripe = useStripe()
+    const elements = useElements()
+
+    const createSubscription = async () => {
+        try {
+            
+        } catch (err) {
+            console.log(err)
+            alert('payment failed', + err.message)
+        }
+    } 
+
   return (
-    <div>PaymentForm</div>
+    <div style={{ width: "40%" }}>
+      Name:{" "}
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      Email:{" "}
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <br />
+      <CardElement />
+      <br />
+      <button onClick={createSubscription}>Subscribe</button>
+    </div>
   )
 }
 
 export default PaymentForm
+
